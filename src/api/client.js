87a6832bc0532f72b9532b5a5617784d408c9ef9
ManojVeeraSrinivasKolla <152895203+ManojVeeraSrinivasKolla@users.noreply.github.com>
2025-03-20 15:49:10 +0530

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const client = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL
     ? `${import.meta.env.VITE_BASE_URL}/api`
-    : "http://13.53.111.184:8000/api",
+    : "http://51.20.249.8:8000/api",
 });
 
 export default client;
